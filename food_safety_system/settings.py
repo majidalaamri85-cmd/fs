1d847f0214d4d144f77b66f468362206978e3b66
Food Safety Team <team@example.com>
2026-05-07 17:12:09 +0400

    Fix static asset paths and add logo

--- food_safety_system/settings.py
+++ food_safety_system/settings.py
@@ -84,7 +84,7 @@ TIME_ZONE = 'Asia/Muscat'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
